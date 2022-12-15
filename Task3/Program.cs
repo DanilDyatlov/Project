f2748ed9d9d79df306551b6395fa69f7ca1d27be
danil <netrow@gmail.com>
2022-12-16 01:15:46 +0500

    Задача 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,32 +4,32 @@
 // 5 -> Пятница
 
 Console.WriteLine("Введите целое число");
-string a = (Console.ReadLine());
-if (a == "1")
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number == 1)
 {
     Console.WriteLine("Понедельник"); //
 }
-else if (a == "2")
+else if (number == 2)
 {
      Console.WriteLine("Вторник");
 }
-else if (a == "3")
+else if (number == 3)
 {
     Console.WriteLine("Среда");
 }
-else if (a == "4")
+else if (number == 4)
 {
     Console.WriteLine("Четверг");
 }
-else if (a == "5")
+else if (number == 5)
 {
     Console.WriteLine("Пятница");
 }
-else if (a == "6")
+else if (number == 6)
 {
     Console.WriteLine("Суббота");
 }
-else if (a == "7")
+else if (number == 7)
 {
     Console.WriteLine("Воскресенье");
 }
